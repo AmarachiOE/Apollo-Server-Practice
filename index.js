92c6deb389862dd2bb0e389fe63ef41a183cbe49
AmarachiOE <ao.ejiawoko@gmail.com>
2019-09-19 13:27:20 -0700

    define resolver

--- index.js
+++ index.js
@@ -36,3 +36,17 @@ const books = [
       author: 'Michael Crichton',
     },
   ];
+
+
+
+// Define resolver
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+
