2f6b750f42e4f04ce32fb3f59cf473a079073c23
AmarachiOE <ao.ejiawoko@gmail.com>
2019-09-19 13:23:59 -0700

    defined schema and dataset

---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Define schema
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+// Define your dataset (can come from a database, a REST API, a static object storage service, or even another GraphQL server)
+
+const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
+    },
+  ];
